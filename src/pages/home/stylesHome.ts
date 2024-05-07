e7e4043e15d38b1275cc72c66afe90c4aa0145a1
Ademir Stefanski <ademirstefanski@gmail.com>
2024-05-07 17:22:23 -0300

    responsive category bar creation

--- src/pages/home/stylesHome.ts
+++ src/pages/home/stylesHome.ts
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
+export const MainContainer = styled.div<{ openMenu: boolean}>`
+ width: 100%;
+  max-width: 1600px;
+  display: flex;
+  flex-direction: column;
+  padding: ${({openMenu}) => openMenu? '140px 50px 0 290px' : '140px 10px 0 100px'};
+  box-sizing: border-box;
+`;
+
 export const Container  = styled.div<{ openMenu: boolean}>`
   width: 100%;
   max-width: 1600px;
